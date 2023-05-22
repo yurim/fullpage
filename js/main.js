6e5e586781952240f751bb9bac0c6dcf61b70f47
yurim <nogod829@gmail.com>
2023-05-22 16:16:53 +0900

    해더 gnb영역 완료

--- js/main.js
+++ js/main.js
@@ -7,3 +7,20 @@ $(function (){
        menu:'.rightNav',
    });
 });
+
+// GNB
+$(function (){
+   setGnb();
+   function setGnb(){
+       $('.innerHeader .gnb > li > a').on('mouseenter focus', function (){
+          $('.innerHeader').addClass('on');
+          $('.innerHeader .gnb > li > .inner').addClass('on');
+          $('header').css({'border': 'none'});
+       });
+       $('header').on('mouseleave',function (){
+           $('.innerHeader').removeClass('on');
+           $('.innerHeader .gnb > li > .inner').removeClass('on');
+           $('header').css({'border': '1px solid rgba(225,225,225,0.1)'});
+       });
+   }
+});
