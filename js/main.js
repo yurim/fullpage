4ae5f1932195d69c97395f0b3879b37aff5c747c
yurim <nogod829@gmail.com>
2023-10-17 17:24:47 +0900

    색션1 슬라이드 상단 텍스트 완료

--- js/main.js
+++ js/main.js
@@ -24,3 +24,29 @@ $(function (){
        });
    }
 });
+
+// 이미지 슬라이드
+$(function (){
+    $('.visual').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplaySpeed :2800,
+        customPaging: function (slider, i) {
+           let  tit = $(slider.$slider[i]).find('.dot').html();
+           return '<div class="pager-tit" class=" + i + ">' + tit + '</div>';
+        }
+    })
+});
+
+// 이미지 슬라이드 - 넓이 높이 스크립트
+$(function (){
+    let winW = $(window).width();
+    let winH= $(window).height();
+    let list = $('.visual .list');
+    list.css({width: winW + 'px', height: winH + 'px'});
+
+});
